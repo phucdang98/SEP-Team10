4392a865db85f3b25498ed4559b4adc69a139d1a
kinan43 <kuroobj.sensei@gmail.com>
2019-08-16 12:41:39 +0700

    update dashboard , delete , leave , leave peding , index , about, account

---
+++ leave-types.php
@@ -0,0 +1,10 @@
+<?php
+
+ $leave_types = ['annual'=>"Nghỉ thường nhật",'sick'=>"Nghỉ bệnh",'study'=>'Nghỉ để đào tạo','emergency'=>'Nghỉ khẩn cấp',
+        'casual'=>'Nghỉ thường nhật','special'=>'Nghỉ phép đặc biệt','examinations'=>'Nghỉ để kiểm tra',
+        'sports'=>'Nghỉ vì tham gia giải','absense'=>'Nghỉ vì chấn thương',
+        'short_embark_disembark'=>'Nghỉ ngắn hạn',
+        'long_embark_disembark'=>'Nghỉ dài hạn'];
+ 
+ $arr = ['annual','sick','maternity','paternity','study','emergency','casual','special',
+         'examinations','sports','absense','short_embark_disembark','long_embark_disembark'];
